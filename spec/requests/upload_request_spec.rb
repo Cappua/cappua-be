df60a75f6a68e2f1e586435208b013891dbf35b4
Nick King <nickmaxking@gmail.com>
2021-02-20 23:28:20 -0700

    Adapt test with proper form formatting and fastjson adherence
    Co-authored-by: Eugene Theriault <45305677+ETBassist@users.noreply.github.com>

--- spec/requests/upload_request_spec.rb
+++ spec/requests/upload_request_spec.rb
@@ -4,37 +4,51 @@ describe 'Upload endpoint' do # intentionally omitting vcr; see below
   before :each do
     AWSResponse = Struct.new(:etag)
     @mock_response = AWSResponse.new("\"55e5r7b11rr7575f11l36423298e3a22\"")
+    include ActionDispatch::TestProcess::FixtureFile
+    @sample_verse = fixture_file_upload("spec/fixtures/es_zone_in.mp3")
+    # @sample_verse = File.read('spec/fixtures/es_zone_in.mp3')
   end
 
-  it 'can upload to an aws s3 bucket' do
+  it 'can upload a verse to an aws s3 bucket' do
     user = create(:user)
     track = create(:track)
-    form_body = { 
-      audio: 'somefilethatsgoingtobereallylongwhyareyoustillreadingthis',
+    form_body = {
+      audio: @sample_verse,
       userId: user.id ,
-      trackId: track.id
+      trackId: track.id,
+      type: 'verse'
     }
 
-    # TODO: modify the arguments in the .with statement to match what we're 
+    # TODO: modify the arguments in the .with statement to match what we're
     # using in the actual controller, or this mock will fail
     # ALSO WARNING: I'm not setting up vcr for this test because I don't want to record
     # the response from AWS; it may contain sensitive data
-    allow_any_instance_of(Aws::S3::Client).to receive(:put_object).with({
-      bucket: 'some_bucket',
-      key: 'file_prefix',
-      body: form_body[:audio]
-    }).and_return(@mock_response)
+    allow_any_instance_of(Aws::S3::Client).to receive(:put_object).and_return(@mock_response)
+
+    # with({
+    #   bucket: 'cappuatracks',
+    #   key: 'verses/es_zone_in.mp3',
+    #   body: form_body[:audio]
+    # }).
 
     post '/upload', params: form_body, headers: { "Content-Type": 'multipart/form-data' }
+    json = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    expect(response.body).to have_key(:data)
-    expect(response.body[:data]).to have_key(:verse)
-    expect(response.body[:data][:verse]).to have_key(:audio_path)
-    expect(response.body[:data][:verse][:audio_path]).to be_a(String)
-    expect(response.body[:data][:verse]).to have_key(:track_id)
-    expect(response.body[:data][:verse][:track_id]).to eq(track.id)
-    expect(response.body[:data][:verse]).to have_key(:user_id)
-    expect(response.body[:data][:verse][:user_id]).to eq(user.id)
+    expect(json).to be_a(Hash)
+    expect(json).to have_key(:data)
+    expect(json[:data]).to be_a(Hash)
+    expect(json[:data]).to have_key(:id)
+    expect(json[:data][:id]).to be_a(String)
+    expect(json[:data]).to have_key(:type)
+    expect(json[:data][:type]).to be_a(String)
+    expect(json[:data]).to have_key(:attributes)
+    expect(json[:data][:attributes]).to be_a(Hash)
+    expect(json[:data][:attributes]).to have_key(:audio_path)
+    expect(json[:data][:attributes][:audio_path]).to be_a(String)
+    expect(json[:data][:attributes]).to have_key(:track_id)
+    expect(json[:data][:attributes][:track_id]).to eq(track.id)
+    expect(json[:data][:attributes]).to have_key(:user_id)
+    expect(json[:data][:attributes][:user_id]).to eq(user.id)
   end
 end
