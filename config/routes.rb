fe1acd7b9709901fbeaad66867bdceebcac5f093
Nick King <nickmaxking@gmail.com>
2021-02-20 20:19:36 -0700

    Change upload new action to upload create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post "/upload", to: "upload#new"
+  post "/upload", to: "upload#create"
 end
