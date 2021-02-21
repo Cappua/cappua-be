e64f6e5c95abf147cf142fef84e8dbf5aed3aa10
Eugene Theriault <hybridbassist@gmail.com>
2021-02-20 18:41:56 -0800

    Update stub of aws method to accept correct args
    Should now be able to accept three arguments, though the values of that
    method will need to be updated to match what we're actually doing in the
    controller

--- spec/requests/upload_request_spec.rb
+++ spec/requests/upload_request_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 describe 'Upload endpoint' do
   before :each do
     AWSResponse = Struct.new(:etag)
-    mock_response = AWSResponse.new("\"55e5r7b11rr7575f11l36423298e3a22\"")
-    allow_any_instance_of(Aws::S3::Client).to receive(:put_object).with(:bucket, :key, :body).and_return(mock_response)
+    @mock_response = AWSResponse.new("\"55e5r7b11rr7575f11l36423298e3a22\"")
   end
 
   it 'can upload to an aws s3 bucket' do
@@ -15,6 +14,14 @@ describe 'Upload endpoint' do
       userId: user.id ,
       trackId: track.id
     }
+
+    # TODO: modify the arguments in the .with statement to match what we're 
+    # using in the actual controller, or this mock will fail
+    allow_any_instance_of(Aws::S3::Client).to receive(:put_object).with({
+      bucket: 'some_bucket',
+      key: 'file_prefix',
+      body: form_body[:audio]
+    }).and_return(@mock_response)
 
     post '/upload', params: form_body, headers: { "Content-Type": 'multipart/form-data' }
 
