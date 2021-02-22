fcddc57154d446efc010344048f4e4966a7ec680
the-color-bliu <brian.b.liu@gmail.com>
2021-02-22 15:04:48 -0700

    Made changes to DB
    Co-authored-by: Sage Gonzales <65799246+SageOfCode@users.noreply.github.com>
    Co-authored-by: Eugene Theriault <45305677+ETBassist@users.noreply.github.com>
    Co-authored-by: Nick King <68172332+nmking22@users.noreply.github.com>

--- app/serializers/verse_serializer.rb
+++ app/serializers/verse_serializer.rb
@@ -1,4 +1,4 @@
 class VerseSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :audio_path, :track_id, :user_id, :title
+  attributes :id, :audio_path, :competition_id, :user_id, :title
 end
