b5bcfb5f11d6fa1334f12a63e5a61ffa46254acb
Eugene Theriault <hybridbassist@gmail.com>
2021-02-22 16:21:39 -0800

    Create test for getting all verses through graphql
    Co-authored-by: Nick King <68172332+nmking22@users.noreply.github.com>

--- spec/requests/queries/verse_query_spec.rb
+++ spec/requests/queries/verse_query_spec.rb
@@ -4,7 +4,7 @@ describe 'A graphql query to get a single verse' do
   it 'can get a successful response with all fields' do
     verse = create(:verse)
 
-    query_string = " query {
+    query_string = "query {
       verse(id: #{verse.id}) {
         id
         audioPath
@@ -31,5 +31,37 @@ describe 'A graphql query to get a single verse' do
     expect(result[:data][:verse][:userId]).to eq(verse.user_id)
     expect(result[:data][:verse]).to have_key(:title)
     expect(result[:data][:verse][:title]).to eq(verse.title)
+  end
+end
+
+describe 'A graphql query to get multiple verses' do
+  it 'returns an array of verses' do
+    create_list(:verse, 4)
+
+    query_string = "query {
+      verses {
+        id
+        audioPath
+        competitionId
+        userId
+        title
+      }
+    }"
+
+    post graphql_path, params: { query: query_string }
+
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(result).to have_key(:data)
+    expect(result[:data]).to have_key(:verses)
+    expect(result[:data][:verses].length).to eq(4)
+    result[:data][:verses].each do |verse|
+      expect(verse).to have_key(:id)
+      expect(verse).to have_key(:audioPath)
+      expect(verse).to have_key(:competitionId)
+      expect(verse).to have_key(:userId)
+      expect(verse).to have_key(:title)
+    end
   end
 end
