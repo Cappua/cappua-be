16587fa92ad3d97730f4afb0b9102163a18ecfbf
Nick King <nickmaxking@gmail.com>
2021-02-24 15:52:45 -0700

    Add seeds for users, verses, competition and votes
    Co-authored-by: Sage Gonzales <65799246+SageOfCode@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,58 @@
 
 user = User.create(
   name: 'Biz Markie',
-  email: 'justafriend@example.com'
+  email: 'justafriend@example.com',
+  image: 'https://media2.fdncms.com/riverfronttimes/imager/u/slideshow/2654166/bizmarkie.jpg'
+)
+
+competition = Competition.create(
+  track_path: '/competitions/nu_metal_jamboree',
+  month: 2,
+  year: 2021,
+  description: 'Fred Durst and Stuff',
+  genre: 'Nu Metal Jamboree',
+  rules: '4 weeks'
+)
+
+10.times do |index|
+  User.create(
+    name: Faker::Games::Witcher.character,
+    email: Faker::Internet.email,
+    image: Faker::Internet.url
+  )
+end
+
+10.times do |index|
+  Verse.create(
+    audio_path: "verses/verse_#{index}",
+    user_id: (index + 2),
+    title: "verse_#{index}",
+    competition: competition
+  )
+end
+
+10.times do |index|
+  Vote.create(
+    user_id: (index + 2),
+    verse_id: (index + 1)
+  )
+end
+
+3.times do |index|
+  Vote.create(
+    user_id: (index + 2),
+    verse_id: 9
+  )
+end
+
+2.times do |index|
+  Vote.create(
+    user_id: (index + 2),
+    verse_id: 8
+  )
+end
+
+Vote.create(
+  user: user,
+  verse_id: 7
 )
