d8b279902f2e80cfd1b1aeb221514b58411c752f
Sage Gonzales <sagegonzales15@gmail.com>
2021-02-25 16:36:00 -0700

    Alter test for single competition to allow for nil
    Co-authored-by: Brian Liu <67594471+the-color-bliu@users.noreply.github.com>
    Co-authored-by: Eugene Theriault <45305677+ETBassist@users.noreply.github.com>

--- spec/requests/queries/competition_query_spec.rb
+++ spec/requests/queries/competition_query_spec.rb
@@ -18,23 +18,32 @@ describe 'It can get a single competition' do
 
     post graphql_path, params: { query: query_string }
     result = JSON.parse(response.body, symbolize_names: true)
-    expect(result).to eq({
-                          'data': {
-                            'competition': {
-                              'id': "#{competition.id}",
-                              'trackPath': "#{competition.track_path}",
-                              'month': competition.month,
-                              'year': competition.year,
-                              'description': "#{competition.description}",
-                              'genre': "#{competition.genre}",
-                              'rules': "#{competition.rules}",
-                              'image': nil,
-                              'verses': [
-                                {'id': "#{verses[0].id}"},
-                                {'id': "#{verses[1].id}"}
-                              ]
-                            }
-                          }
-                        })
+    expect(response).to be_successful
+
+    # require 'pry'; binding.pry
+    expect(result).to have_key(:data)
+    expect(result[:data]).to be_a(Hash)
+    expect(result[:data]).to have_key(:competition)
+    expect(result[:data][:competition]).to be_an(Hash)
+
+    comp_data = result[:data][:competition]
+    expect(comp_data).to have_key(:id)
+    expect(comp_data[:id]).to be_a(String)
+    expect(comp_data).to have_key(:trackPath)
+    expect(comp_data[:trackPath]).to be_a(String)
+    expect(comp_data).to have_key(:month)
+    expect(comp_data[:month]).to be_a(Numeric)
+    expect(comp_data).to have_key(:year)
+    expect(comp_data[:year]).to be_a(Numeric)
+    expect(comp_data).to have_key(:description)
+    expect(comp_data[:description]).to be_a(String)
+    expect(comp_data).to have_key(:genre)
+    expect(comp_data[:genre]).to be_a(String)
+    expect(comp_data).to have_key(:rules)
+    expect(comp_data[:rules]).to be_a(String)
+    expect(comp_data).to have_key(:image)
+    expect(comp_data[:image]).to eq(competition.image)
+    expect(comp_data).to have_key(:verses)
+    expect(comp_data[:verses]).to be_an(Array)
   end
 end
