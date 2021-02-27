dfcc5788046fb0fab56225ed97c3455fbac2511b
Sage Gonzales <sagegonzales15@gmail.com>
2021-02-27 16:57:32 -0700

    Change data type and set null for user image
    Co-authored-by: Cooper Terrones <67164959+coopterrones@users.noreply.github.com>
    Co-authored-by: Damaceo Vincent Howard <62954974+demaceo@users.noreply.github.com>
    Co-authored-by: Eugene Theriault <45305677+ETBassist@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_230329) do
+ActiveRecord::Schema.define(version: 2021_02_27_235430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 2021_02_27_230329) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.binary "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_sub"
+    t.string "image", default: "null"
   end
 
   create_table "verses", force: :cascade do |t|
