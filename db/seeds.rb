e2dd88efc8d111194f2726f8125943f57fddc485
Nick King <nickmaxking@gmail.com>
2021-03-01 13:48:17 -0700

    Change feb audio paths to relevant s3 bucket names

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,13 +40,13 @@ march_competition = Competition.create(
   track_path: '/competitions/march_wu_tang_forever.mp3',
   month: 3,
   year: 2021,
-  description: "This month's theme is East Coast Hip Hop. Bring your Wu Tang style to this one.",
+  description: "Wu Tang Sword Style. Get with your sick tight click, and go all out.",
   genre: 'East Coast Hip Hop',
-  rules: 'Verse submissions are due by March 14th. Final vote tally and winner announcement on March 31st. Rappers can edit track, but most stay within 16 bars.',
+  rules: 'Verse submissions are due by March 14th. Final vote tally and winner announcement on April 1st. Rappers can edit the track, but most stay within 16 bars.',
   image: 'https://i.ytimg.com/vi/5CzsXvAZ6R4/mqdefault.jpg'
 )
 
-10.times do |index|
+9.times do |index|
   word = Faker::Hipster.words[0].capitalize
   word_2 = Faker::Hipster.words[1].capitalize
   Verse.create(
@@ -57,7 +57,7 @@ march_competition = Competition.create(
   )
 end
 
-10.times do |index|
+9.times do |index|
   Vote.create(
     user_id: (index + 2),
     verse_id: (index + 1)
@@ -85,27 +85,34 @@ Vote.create(
 
 # Create March competition, verses, votes
 feb_competition = Competition.create(
-  track_path: '/competitions/feb_some_comp.mp3',
+  track_path: '/competitions/feb_run_the_jewels_out_of_sight.mp3',
   month: 2,
   year: 2021,
-  description: "This month's theme is East Coast Hip Hop. Bring your Wu Tang style to this one.",
+  description: "RUN THEM JEWELS FAST. Make Killer Mike and El-P proud with your verses.",
   genre: 'East Coast Hip Hop',
-  rules: 'Verse submissions are due by March 14th. Final vote tally and winner announcement on March 31st. Rappers can edit track, but most stay within 16 bars.',
-  image: 'https://i.ytimg.com/vi/5CzsXvAZ6R4/mqdefault.jpg'
+  rules: 'Verse submissions are due by February 14th. Final vote tally and winner announcement on March 1st. Rappers can edit the track, but most stay within 16 bars.',
+  image: 'https://cdn.vox-cdn.com/thumbor/JOFn2f6bZY2EsUmyxF1GrxGSXjw=/0x0:1548x1024/1200x800/filters:focal(627x76:873x322)/cdn.vox-cdn.com/uploads/chorus_image/image/66314789/run_the_jewels_march_2017_billboard_1548.0.jpg'
 )
 
-10.times do |index|
+def zero_to_three(number)
+  until 0 <= number && number < 3
+    number -= 3
+  end
+  number
+end
+
+9.times do |index|
   word = Faker::Hipster.words[0].capitalize
   word_2 = Faker::Hipster.words[1].capitalize
   Verse.create(
-    audio_path: "/verses/2021/03/verse_#{index}.mp3",
+    audio_path: "/verses/2021/02/verse_#{zero_to_three(index)}.mp3",
     user_id: (index + 2),
     title: "#{word} #{word_2}",
     competition: feb_competition
   )
 end
 
-10.times do |index|
+9.times do |index|
   Vote.create(
     user_id: (index + 2),
     verse_id: (index + 10)
@@ -115,7 +122,7 @@ end
 3.times do |index|
   Vote.create(
     user_id: (index + 2),
-    verse_id: 18
+    verse_id: 16
   )
 end
 
@@ -128,7 +135,7 @@ end
 
 Vote.create(
   user: user,
-  verse_id: 16
+  verse_id: 18
 )
 
 # Create January competition, verses, votes
@@ -136,24 +143,24 @@ jan_competition = Competition.create(
   track_path: '/competitions/jan_some_comp.mp3',
   month: 1,
   year: 2021,
-  description: "This month's theme is East Coast Hip Hop. Bring your Wu Tang style to this one.",
+  description: "RUN THEM JEWELS FAST. Make Killer Mike and El-P proud with your verses.",
   genre: 'East Coast Hip Hop',
   rules: 'Verse submissions are due by March 14th. Final vote tally and winner announcement on March 31st. Rappers can edit track, but most stay within 16 bars.',
   image: 'https://i.ytimg.com/vi/5CzsXvAZ6R4/mqdefault.jpg'
 )
 
-10.times do |index|
+9.times do |index|
   word = Faker::Hipster.words[0].capitalize
   word_2 = Faker::Hipster.words[1].capitalize
   Verse.create(
     audio_path: "/verses/2021/01/verse_#{index}.mp3",
     user_id: (index + 2),
     title: "#{word} #{word_2}",
-    competition: feb_competition
+    competition: jan_competition
   )
 end
 
-10.times do |index|
+9.times do |index|
   Vote.create(
     user_id: (index + 2),
     verse_id: (index + 10)
