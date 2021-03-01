eef5086c3fc2e4dc2c21c7be9fb552b64091c98e
Nick King <nickmaxking@gmail.com>
2021-02-28 20:00:03 -0700

    Add tests for nested audio file locations

--- spec/facades/verse_facade_spec.rb
+++ spec/facades/verse_facade_spec.rb
@@ -4,6 +4,7 @@ describe 'verse facade' do
   AWSResponse = Struct.new(:etag)
   before :each do
     @audio = fixture_file_upload("spec/fixtures/es_zone_in.mp3")
+    allow(Date).to receive(:today).and_return(Date.new(2021,3,1))
   end
 
   it 'successful upload serializes verse' do
@@ -25,7 +26,7 @@ describe 'verse facade' do
 
     expect(verse).to be_a(Verse)
     expect(verse.id).to be_a(Integer)
-    expect(verse.audio_path).to eq("verses/es_zone_in.mp3")
+    expect(verse.audio_path).to eq("verses/2021/03/es_zone_in.mp3")
     expect(verse.user_id).to eq(user.id)
     expect(verse.competition_id).to eq(competition.id)
     expect(verse.title).to eq('Just a Friend')
@@ -51,5 +52,13 @@ describe 'verse facade' do
     expect(output).to be_a(Hash)
     expect(output).to have_key(:error)
     expect(output[:error]).to eq('Verse could not be successfully uploaded to cloud database.')
+  end
+
+  it 'current_month' do
+    expect(VerseFacade.current_month).to eq('03')
+  end
+
+  it 'current_year' do
+    expect(VerseFacade.current_year).to eq('2021')
   end
 end
