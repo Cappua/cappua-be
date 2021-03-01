8d60a05624edd9e1a345c9f2158dd9609db3f390
Nick King <nickmaxking@gmail.com>
2021-02-28 20:14:09 -0700

    Add jan/feb competitions

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,30 @@ user = User.create(
   image: 'https://media2.fdncms.com/riverfronttimes/imager/u/slideshow/2654166/bizmarkie.jpg'
 )
 
+jan_competition = Competition.create(
+  track_path: 'competitions/march_wu_tang_forever.mp3',
+  month: 1,
+  year: 2021,
+  description: "This month's theme is East Coast Hip Hop. Bring your Wu Tang style to this one.",
+  genre: 'East Coast Hip Hop',
+  rules: 'Verse submissions are due by March 14th. Final vote tally and winner announcement on March 31st. Rappers can edit track, but most stay within 16 bars.',
+  image: 'https://i.ytimg.com/vi/5CzsXvAZ6R4/mqdefault.jpg'
+  # verse_instrumental: '/competitions/march_wu_tang_forever_instrumental_verse.mp3'
+)
+
+feb_competition = Competition.create(
+  track_path: 'competitions/jan_some_comp.mp3',
+  month: 2,
+  year: 2021,
+  description: "This month's theme is East Coast Hip Hop. Bring your Wu Tang style to this one.",
+  genre: 'East Coast Hip Hop',
+  rules: 'Verse submissions are due by March 14th. Final vote tally and winner announcement on March 31st. Rappers can edit track, but most stay within 16 bars.',
+  image: 'https://i.ytimg.com/vi/5CzsXvAZ6R4/mqdefault.jpg'
+  # verse_instrumental: '/competitions/march_wu_tang_forever_instrumental_verse.mp3'
+)
+
 march_competition = Competition.create(
-  track_path: '/competitions/march_wu_tang_forever.mp3',
+  track_path: 'competitions/feb_some_comp.mp3',
   month: 3,
   year: 2021,
   description: "This month's theme is East Coast Hip Hop. Bring your Wu Tang style to this one.",
@@ -26,7 +48,7 @@ march_competition = Competition.create(
 
 10.times do |index|
   User.create(
-    name: Faker::Games::Witcher.character,
+    name: Faker::Cannabis.strain,
     email: Faker::Internet.email,
     user_sub: "mosk_sub",
     image: "https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg"
@@ -35,10 +57,10 @@ end
 
 10.times do |index|
   Verse.create(
-    audio_path: "verses/verse_#{index}",
+    audio_path: "verses/2021/03/verse_#{index}.mp3",
     user_id: (index + 2),
     title: "verse_#{index}",
-    competition: competition
+    competition: march_competition
   )
 end
 
