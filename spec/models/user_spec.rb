3eef0b22017776805f013476c63d637727fccd11
Nick King <nickmaxking@gmail.com>
2021-03-01 11:43:50 -0700

    Add uniqueness validation test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  describe 'validations' do
+    it do
+      should validate_uniqueness_of :email
+    end
+  end
+  
   describe 'relationships' do
     it do
       should have_many :verses
