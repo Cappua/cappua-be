b1ff9285d4807231b95972e63b9d8d70bf67110f
Sage Gonzales <sagegonzales15@gmail.com>
2021-03-01 20:27:26 -0700

    Add validations to competition
    Co-authored-by: Brian Liu <67594471+the-color-bliu@users.noreply.github.com>
    Co-authored-by: Cooper Terrones <67164959+coopterrones@users.noreply.github.com>
    Co-authored-by: Damaceo Vincent Howard <62954974+demaceo@users.noreply.github.com>
    Co-authored-by: Eugene Theriault <45305677+ETBassist@users.noreply.github.com>
    Co-authored-by: Nick King <68172332+nmking22@users.noreply.github.com>

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,3 +1,10 @@
 class Competition < ApplicationRecord
+  validates_presence_of :track_path, 
+                        :month, 
+                        :year, 
+                        :description, 
+                        :genre, 
+                        :rules, 
+                        :image 
   has_many :verses
 end
