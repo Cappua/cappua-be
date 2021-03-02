a1d7505ae0663301905e263c454d10bfe0be6d2d
Sage Gonzales <sagegonzales15@gmail.com>
2021-03-01 20:24:57 -0700

    Add validation tests for vote table

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 describe Vote, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of(:verse_id)}
+    it {should validate_presence_of(:user_id)}
+  end
   describe 'relationships' do
     it do
       should belong_to :verse
       should belong_to :user
       should belong_to(:user_vote).class_name('Verse')
-    end
-  end
-
-  describe 'validations' do
-    it do
     end
   end
 end
