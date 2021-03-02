78fceb3a673f3305ee9fe7745dfa02757706dc69
Nick King <nickmaxking@gmail.com>
2021-03-01 20:58:15 -0700

    Add title field to competition type query

--- app/graphql/types/competition_type.rb
+++ app/graphql/types/competition_type.rb
@@ -8,6 +8,7 @@ module Types
     field :genre, String, null: true
     field :rules, String, null: true
     field :image, String, null: true
+    field :title, String, null: true
     field :verses, [Types::VerseType], null: true
   end
 end
